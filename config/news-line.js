14a5aad6f12676b1ed372d012e31c2647bd84187
master1moon <98053779+master1moon@users.noreply.github.com>
2025-09-27 06:49:37 +0300

    Add files via upload

---
+++ config/news-line.js
@@ -0,0 +1,1 @@
+setConfig(document.currentScript,"")
---
+++ config/profiles.js
@@ -0,0 +1,4 @@
+setConfig(document.currentScript,'{"price": "100 ريال","time": "4 ساعات","transfer": "200 ميجا بايت","validity": "3 أيام"}')
+setConfig(document.currentScript,'{"price": "100 ريال","time": "4 ساعات","transfer": "200 ميجا بايت","validity": "3 أيام"}')
+setConfig(document.currentScript,'{"price": "100 ريال","time": "4 ساعات","transfer": "200 ميجا بايت","validity": "3 أيام"}')
+setConfig(document.currentScript,'profiles')
